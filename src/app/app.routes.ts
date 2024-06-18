6f988ff59478b6123eae4fbefde0995e2d117124
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-06-18 15:53:51 -0400

    Agrega bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { UsersComponent } from "./pages/users/users.component";
+import { ContactoComponent } from "./pages/contacto/contacto.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "usuarios", component: UsersComponent },
+  { path: "contacto", component: ContactoComponent },
+  { path: "**", redirectTo: "", pathMatch: "full" },
+];
